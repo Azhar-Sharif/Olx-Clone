ae366ed68f50f2d823f6dc57bb8a2d0cc4613973
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2025-10-27 06:12:18 -0700

    feat: register user in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,61 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("username", "email", "role", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+    search_fields = ("username", "email", "phone_no")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "phone_no",
+                    "address",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "role",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "role",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
