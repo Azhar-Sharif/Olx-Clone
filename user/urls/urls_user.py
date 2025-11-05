18eb528ffebd8b88e747ecf50c8eaf1572add2d2
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2025-11-05 01:54:50 -0800

    feat: register routes for user

---
+++ user/urls/urls_user.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from user.views import UserCreateView, UserProfileView
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="user-register"),
+    path("profile/", UserProfileView.as_view(), name="user-profile"),
+]
