507bdb771e099e3b6162a12ae91afc808cb4e1ff
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2025-11-06 04:06:34 -0800

    fix: update imports

--- catalog/models/__init__.py
+++ catalog/models/__init__.py
@@ -1,3 +1,3 @@
 from catalog.models.category import Category
-from catalog.models.order import Order
-from catalog.models.product import Product
+from catalog.models.orders import Order
+from catalog.models.products import Product
--- catalog/views/__init__.py
+++ catalog/views/__init__.py
@@ -1,3 +1,3 @@
-from catalog.views.category_view import CategoryViewSet
+from catalog.views.category_view import CategoryDetailView, CategoryListView
 from catalog.views.order_view import OrderViewSet
 from catalog.views.product_views import ProductViewSet
