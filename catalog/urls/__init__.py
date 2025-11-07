a1a62f6bf0283932ed457828f7639a3d8e29f90a
Azhar <m.azhar@innovaxel.com>
2025-11-07 15:41:25 +0500

    fix: Allow anyone to retrieve categories

--- catalog/urls/__init__.py
+++ catalog/urls/__init__.py
@@ -1,3 +1,11 @@
+from django.urls import include, path
+
 from catalog.urls.category_urls import urlpatterns
 from catalog.urls.order_urls import urlpatterns
 from catalog.urls.products_url import urlpatterns
+
+urlpatterns = [
+    path("", include("catalog.urls.category_urls")),
+    path("", include("catalog.urls.order_urls")),
+    path("", include("catalog.urls.products_url")),
+]
