d5fbcbfa8dc2c04d2b40f4358c489da756e1b213
Azhar <m.azhar@innovaxel.com>
2025-11-07 15:49:18 +0500

    refactor: define urlpatterns in __init__ file

--- catalog/urls/__init__.py
+++ catalog/urls/__init__.py
@@ -1,9 +1,5 @@
 from django.urls import include, path
 
-from catalog.urls.category_urls import urlpatterns
-from catalog.urls.order_urls import urlpatterns
-from catalog.urls.products_url import urlpatterns
-
 urlpatterns = [
     path("", include("catalog.urls.category_urls")),
     path("", include("catalog.urls.order_urls")),
