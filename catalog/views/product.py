8282609467ecfd0e484996e2a5086526524843f1
Azhar <m.azhar@innovaxel.com>
2025-11-13 16:38:16 +0500

    refactor: remove redundant perform_update method

--- catalog/views/product.py
+++ catalog/views/product.py
@@ -17,7 +17,3 @@ class ProductViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
 
         serializer.save(user=self.request.user)
-
-    def perform_update(self, serializer):
-
-        serializer.save(user=self.request.user)
