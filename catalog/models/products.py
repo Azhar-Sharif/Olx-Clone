e83eb38e63033cf120889020091ee248297215d0
Azhar <m.azhar@innovaxel.com>
2025-11-13 15:02:02 +0500

    refactor: change Product.quantity default from 0 to 1

--- catalog/models/products.py
+++ catalog/models/products.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(
-        default=0, help_text="Available stock quantity"
+        default=1, help_text="Available stock quantity"
     )
     product_img = models.ImageField(
         upload_to="products/", blank=True, null=True
