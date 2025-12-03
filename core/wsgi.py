3d466536cd9474183fbd1605136fb4561a749fa4
Azhar <m.azhar@innovaxel.com>
2025-12-03 23:19:49 +0500

    feat: simplify ASGI/WSGI env-based config and logging

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,14 +1,24 @@
 """
 WSGI config for core project.
 
-It exposes the WSGI callable as a module-level variable
-named ``application``.
+It exposes the WSGI callable as a module-level
+variable named ``application``.
 """
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+logger = logging.getLogger(__name__)
+
+env = os.getenv("DJANGO_ENV", "local").lower()
+
+if env == "local":
+    logger.info("WSGI - Running in local environment")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"core.settings.{env}")
+
+logger.info("WSGI - Settings module: %s", os.environ["DJANGO_SETTINGS_MODULE"])
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -1,11 +1,21 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import logging
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+    from dotenv import load_dotenv
+    load_dotenv()
+    
+    django_env = os.getenv("DJANGO_ENV", "local").lower()
+    settings_module = f"core.settings.{django_env}"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.info(f"Django environment: {django_env}")
+    logger.info(f"Settings module: {settings_module}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
