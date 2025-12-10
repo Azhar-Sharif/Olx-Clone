864046b0a9e98b6812ce989d0e210b9d96afc45e
Azhar <m.azhar@innovaxel.com>
2025-12-10 17:26:35 +0500

    fix: correct the ordering of setting.py variables

--- core/settings/base.py
+++ core/settings/base.py
@@ -31,6 +31,16 @@ INSTALLED_APPS = [
     "catalog",
     "utils",
 ]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = os.getenv(
+    "CORS_ALLOWED_ORIGINS",
+    "http://localhost:3000,http://localhost:8000,http://127.0.0.1:3000",
+).split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
+    "http://localhost:3000,http://localhost:8000,http://127.0.0.1:3000",
+).split(",")
+
 
 MIDDLEWARE = [
     "middlewares.request_id.RequestIdMiddleware",
--- core/settings/local.py
+++ core/settings/local.py
@@ -17,13 +17,12 @@ DATABASES = {
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = os.getenv(
-    "CSRF_TRUSTED_ORIGINS",
-    "http://localhost:3000,http://localhost:8000,http://127.0.0.1:3000",
-).split(",")
-
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = os.getenv(
     "CORS_ALLOWED_ORIGINS",
+    "http://localhost:3000,http://localhost:8000,http://127.0.0.1:3000",
+).split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
     "http://localhost:3000,http://localhost:8000,http://127.0.0.1:3000",
 ).split(",")
